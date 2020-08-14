a1219f44d2bfcc3d8e75b249f989f8efaeb3d5d5
Julian Osorio <julian.osorior@gmail.com>
2020-08-13 21:16:18 -0500

    - Initial project set up.

---
+++ moonphases/moonphases/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  moonphases
+//
+//  Created by techtalks on 13/08/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ moonphases/moonphases/moonphasesApp.swift
@@ -0,0 +1,17 @@
+//
+//  moonphasesApp.swift
+//  moonphases
+//
+//  Created by techtalks on 13/08/20.
+//
+
+import SwiftUI
+
+@main
+struct moonphasesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
