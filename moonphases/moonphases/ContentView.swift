409a1afef0fbfafc0343422e756ed77c6a7cd0ed
Julián Osorio <julian.osorior@gmail.com>
2020-10-19 22:36:19 -0500

    - Connect view model with view.

--- moonphases/moonphases/ContentView.swift
+++ moonphases/moonphases/ContentView.swift
@@ -9,11 +9,21 @@ import SwiftUI
 
 struct ContentView: View {
     
+    @State private var viewModel: MoonPhaseViewModel?
     var body: some View {
         VStack {
             Text("Moon Phase").padding()
-        }
+            Text(viewModel?.title ?? "")
+        }.onAppear(perform: {
+            self.loadViewModel()
+        })
+    }
+    
+    private func loadViewModel() {
+        let date = Date()
+        let moonPhase = MoonPhaseFacade.moonPhaseOn(date: date)
         
+        self.viewModel = MoonPhaseViewModel(moonPhaseForDate: moonPhase)
     }
 }
 
